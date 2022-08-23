fe0c14f213839a7701f7c11689e411f6cdfa12a0
Hana Stupková <hstupkova@gmail.com>
2022-08-23 22:30:15 +0200

    surrender, hiding controls, starting new game

--- app.js
+++ app.js
@@ -14,9 +14,15 @@ const app = Vue.createApp({
 
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.playerHealth + '%' };
     },
     mayUseSpecialAttack() {
@@ -78,6 +84,17 @@ const app = Vue.createApp({
       }
 
       this.attackPlayer();
+    },
+
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
+
+    surrender() {
+      this.winner = 'monster';
     },
   },
 });
